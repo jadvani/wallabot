b7966cd5a4957d142df7a3154e74b44ba2f0f9cd
advani <jiaa1992@hotmail.com>
2018-09-18 09:56:15 +0200

    refactoring some functions, same features

--- es.advani.src/scrapWallapop.py
+++ es.advani.src/scrapWallapop.py
@@ -1,37 +1,8 @@
 # -*- coding: utf-8 -*-
+
 import commonFunctions
 from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt #%matplotlib inline para que vaya saliendo
-from requests import get
-
-searchInputText=raw_input('Dime lo que quieres que busque en WallaPop: ')
-
-url = 'https://es.wallapop.com/search?kws='+commonFunctions.translateWords(searchInputText)+'&catIds=&verticalId='
-print('buscando...')
-response = get(url)
-
-html_soup = BeautifulSoup(response.text, 'html.parser')
-type(html_soup)
-
-product_titles = html_soup.find_all('a', class_ = 'product-info-title')
-product_prices = html_soup.find_all('span', class_ = 'product-info-price')
-product_descriptions = html_soup.find_all('p', class_ = 'product-info-description')
-product_images = html_soup.find_all('img', class_ = 'card-product-image')
-quantity = len(product_titles)
-print('he encontrado '+str(quantity)+' resultados')
-
-if quantity>=0:
-    for x in range(0, quantity):
-        img=commonFunctions.url_to_image(product_images[x]['src'])
-        plt.imshow(img)
-        plt.show()
-        print product_titles[x].get_text()+'->'+product_prices[x].get_text()+'\n'
-        print product_descriptions[x].get_text()[:100]+'(...) '+'\n'
-        raw_input('Siguiente...')
-#%%
-import commonFunctions
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt #%matplotlib inline para que vaya saliendo
+import matplotlib.pyplot as plt #%matplotlib show images inline 
 from requests import get 
 
 class listOfProducts():
@@ -44,11 +15,14 @@ class listOfProducts():
     
 #get all the products available
 def createSoupOfProducts(searchInputText):
-    productListSearch=listOfProducts()
     url = 'https://es.wallapop.com/search?kws='+commonFunctions.translateWords(searchInputText)+'&catIds=&verticalId='
     response = get(url)
     html_soup = BeautifulSoup(response.text, 'html.parser')
     type(html_soup)
+    return html_soup
+
+def fillListOfProducts(html_soup):
+    productListSearch=listOfProducts()
     productListSearch.product_titles = html_soup.find_all('a', class_ = 'product-info-title')
     productListSearch.product_prices = html_soup.find_all('span', class_ = 'product-info-price')
     productListSearch.product_descriptions = html_soup.find_all('p', class_ = 'product-info-description')
@@ -57,6 +31,7 @@ def createSoupOfProducts(searchInputText):
     productListSearch.quantity = len(productListSearch.product_titles)
     return productListSearch
 
+#TODO: correct image colors (minor changes)
 def printSoupProducts(productListSearch):
 
     print('he encontrado '+str(productListSearch.quantity)+' resultados')
@@ -99,4 +74,5 @@ def getLocation(itemUrl):
     zipCode=zipCode.split(',\n')[0]
     city=city.split('0')[-1]
     print (zipCode+','+city)
-        
+#example        
+#printSoupProducts(fillListOfProducts(createSoupOfProducts('iron maiden eddie mcfarlane')))
